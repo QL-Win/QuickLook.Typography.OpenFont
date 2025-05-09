fd77739e8c261829f7752f7b410bee9076d90fa0
ema <mccoy39082@163.com>
2025-05-10 02:10:13 +0800

    Fix method name

--- src/Woff2.cs
+++ src/Woff2.cs
@@ -6,16 +6,16 @@ namespace QuickLook.Typography.OpenFont;
 
 public static class Woff2
 {
-    public static PreviewFontInfo GetFontFamilyName(string path)
+    public static PreviewFontInfo GetFontInfo(string path)
     {
         OurOpenFontSystemSetup.SetupWoff2DecompressFunctions();
         using FileStream fs = new(path, FileMode.Open, FileAccess.Read);
         using BinaryReader input = new(fs);
-        Woff2Reader woffReader = new()
+        Woff2Reader woff2Reader = new()
         {
             DecompressHandler = Woff2DefaultBrotliDecompressFunc.DecompressHandler,
         };
         input.BaseStream.Position = 0;
-        return woffReader.ReadPreview(input);
+        return woff2Reader.ReadPreview(input);
     }
 }
